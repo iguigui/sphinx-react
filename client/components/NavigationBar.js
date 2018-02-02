8d4b21c42990d1b58907eadab885478a98dc3fbb
iguigui <crichard>
2018-02-02 20:08:40 +0000

    course4 done

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default () => {
   return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-		  <a className="navbar-brand" href="#">Sphinx</a>
+		  <Link to="/" className="navbar-brand">Sphinx</Link>
 		  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span className="navbar-toggler-icon"></span>
 		  </button>
 
 		  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-		    <ul className="navbar-nav mr-auto">
-
+		    <ul className="navbar-nav mr-auto navbar-right">
 		      <li className="nav-item">
-		        <a className="nav-link" href="#">Link</a>
+		        <Link to="/signup" className="nav-link">Signup</Link>
 		      </li>
 		    </ul>
 		  </div>
--- client/index.js
+++ client/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch   } from 'react-router-dom';
+
 
 import App from './components/App';
+import Greetings from './components/Greetings';
+import SignupPage from './components/signup/SignupPage';
 
-render((
+render(
   <Router>
-    <div>
-      <Route path="/" component={App} />
-    </div>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Greetings} />
+        <Route path="/signup" component={SignupPage} />
+      </Switch>
+    </App>
   </Router>
-), document.getElementById('app'));﻿
+, document.getElementById('app'));﻿
